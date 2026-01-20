2503e4ed1d197d7f0dbc62e05594e9f226609caa
Erismann Matteo <matteo.erismann@student.vinci.be>
2026-01-20 16:33:58 +0100

    add : connection between froint & back

--- backend/routes/auths.js
+++ backend/routes/auths.js
@@ -1,20 +1,26 @@
+/* eslint-disable no-console */
 const express = require('express');
-const { register, login } = require('../models/users');
+// const { register, login } = require('../models/users');
+const { login } = require('../models/users');
 
 const router = express.Router();
 
 /* Register a user */
 router.post('/register', async (req, res) => {
+  console.log('Register request body:', req.body);
   const username = req?.body?.username?.length !== 0 ? req.body.username : undefined;
   const password = req?.body?.password?.length !== 0 ? req.body.password : undefined;
 
   if (!username || !password) return res.sendStatus(400); // 400 Bad Request
 
-  const authenticatedUser = await register(username, password);
+  console.log('Register user:', { username, password });
 
-  if (!authenticatedUser) return res.sendStatus(409); // 409 Conflict
+  // const authenticatedUser = await register(username, password);
 
-  return res.json(authenticatedUser);
+  // if (!authenticatedUser) return res.sendStatus(409); // 409 Conflict
+
+  // return res.json(authenticatedUser);
+  return res.sendStatus(501); // 501 Not Implemented
 });
 
 /* Login a user */
